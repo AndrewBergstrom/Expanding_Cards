2279770011c919f4a29638508ac254ef48b220a5
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-02-15 16:21:44 -1000

    add / remove active class

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
-    console.log(123)
+    removeActiveClasses()
+   panel.classList.add('active')
   })
-
+console.log(panel)
 })
+
+function removeActiveClasses(){
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
